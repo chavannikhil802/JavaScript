9e91971ecd0415392327b9f16a4badbc45ae6f4a
chavannikhil802 <chavannikhil802@gmail.com>
2023-08-17 14:10:26 +0530

    Video 2: Variables

---
+++ V2/02_variables.js
@@ -0,0 +1,20 @@
+// 1 - A variable is a container that stores value.
+// 2 - The value of a JS variable can be changed during the execution of a JS program
+// Following is the example:-
+
+// Declaring variable a
+let a = 369;
+// Printing variable a in console
+console.log("Initial value of a:- "+a);
+// Printing the value of a on the html page/browser
+document.write("Initial value of a: "+a);
+// Updating the value of a
+a = "a now stores string";
+// Printing the value of a
+console.log("Updated value of a:- "+a);
+// Printing the value of a on the html page/browser. Here, <br> is used to display the output on new line
+document.write("<br>Updated value of a: "+a);
+
+// 3 - JavaScript is a dynamically typed language
+// i.e. you can change the variable type of any JS variable at runtime
+// This property is displayed in the above example
