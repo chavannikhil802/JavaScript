51b4e4d19f333750012e8ff4101d8009f1faedf6
Nikhil Narendra Chavan <chavannikhil802@gmail.com>
2023-08-18 23:15:40 +0530

    Video 6: Adding Arithmetic Operators code in JS file

---
+++ V6/06_operators-expressions.js
@@ -0,0 +1,17 @@
+document.write("<h1>Chp. 6 - JavaScript Tutorial 6: Operators and Expressions</h1>");
+document.write('<hr>');
+
+let a = 18;
+let b = 9;
+
+document.write("a = "+a+"<br>");
+document.write("b = "+b+"<br>");
+document.write("<br>a + b = "+(a+b));
+document.write("<br>a - b = "+(a-b));
+document.write("<br>a * b = "+(a*b));
+document.write("<br>a / b = "+(a/b));
+document.write("<br>a % b = "+(a%b));
+a++;
+document.write("<br>Value of a after post-increment: "+a);
+b--;
+document.write("<br>Value of a after post-decrement: "+b);
