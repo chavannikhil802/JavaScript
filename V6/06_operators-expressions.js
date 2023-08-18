eb50f5f07d9a68717415f253c0945177dfdf6e53
Nikhil Narendra Chavan <chavannikhil802@gmail.com>
2023-08-18 23:20:05 +0530

    Video 6: Starting the coding for Assignment Operators

--- V6/06_operators-expressions.js
+++ V6/06_operators-expressions.js
@@ -1,9 +1,11 @@
 document.write("<h1>Chp. 6 - JavaScript Tutorial 6: Operators and Expressions</h1>");
 document.write('<hr>');
 
+// ARITHMETIC OPERATORS
+
 let a = 18;
 let b = 9;
-
+document.write("<h3>Arithmetic Operators</h3>");
 document.write("a = "+a+"<br>");
 document.write("b = "+b+"<br>");
 document.write("<br>a + b = "+(a+b));
@@ -14,4 +16,11 @@ document.write("<br>a % b = "+(a%b));
 a++;
 document.write("<br>Value of a after post-increment: "+a);
 b--;
-document.write("<br>Value of a after post-decrement: "+b);
+document.write("<br>Value of a after post-decrement: "+b+"<br>");
+document.write("<hr>")
+
+// ASSIGNMENT OPERATORS
+
+let c = 18;
+let d = 9;
+document.write("<h3>Assignment Operators</h3>");
