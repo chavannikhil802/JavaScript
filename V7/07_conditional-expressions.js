108940ccf0a5c935a6379ccd513ecfe8ab287748
chavannikhil802 <chavannikhil802@gmail.com>
2023-08-24 17:27:19 +0530

    Adding Ternary code to js file

--- V7/07_conditional-expressions.js
+++ V7/07_conditional-expressions.js
@@ -1,6 +1,9 @@
 document.write("<h1>Chp. 7 - JavaScript Tutorial 7: Conditional Expressions</h1>");
 document.write("<hr>");
 
+document.write("<h3>If-Else Conditions</h3><br>");
+document.write("Code written for prompt() and alert() functions");
+
 let age = prompt("What is your age?");
 // CONVERTING THE STRING INTO A NUMBER
 age = Number.parseInt(age);
@@ -14,3 +17,7 @@ else if(age>18 && age <70) {
 else {
     alert('Age is not valid');
 }
+document.write("<hr>");
+
+document.write("<h3>Ternary Operator</h3><br>");
+document.write("Your age is ", age>18 && age <70? "valid" : "not valid");
