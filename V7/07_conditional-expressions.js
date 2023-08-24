04dc344960351f7d31c5f0313526906448aab4b1
chavannikhil802 <chavannikhil802@gmail.com>
2023-08-24 17:04:44 +0530

    Adding the if, else, if-else conditions in JS file

--- V7/07_conditional-expressions.js
+++ V7/07_conditional-expressions.js
@@ -2,4 +2,15 @@ document.write("<h1>Chp. 7 - JavaScript Tutorial 7: Conditional Expressions</h1>
 document.write("<hr>");
 
 let age = prompt("What is your age?");
-alert("Your age is "+age);
+// CONVERTING THE STRING INTO A NUMBER
+age = Number.parseInt(age);
+
+if(age<=0) {
+    alert('Age cannot be negative or zero');
+}
+else if(age>18 && age <70) {
+    alert('Age is valid');
+}
+else {
+    alert('Age is not valid');
+}
