b4735e0cfbbe933a383d9662ab38fcdce403a1fc
Nikhil Narendra Chavan <chavannikhil802@gmail.com>
2023-09-30 12:08:18 +0530

    Adding solution to question 2 in JS file

--- V8/08_practice-set.js
+++ V8/08_practice-set.js
@@ -1,9 +1,35 @@
 // QUESTION 1 SOLUTION 
 
-let age = prompt("What is your age?");
-if(age > 10 && age < 20) {
-    console.log("Your age lies between 10 and 20");
-}
-else {
-    console.log("Your age is not between 10 and 20");
+// let age = prompt("What is your age?");
+// age = Number.parseInt(age);s
+// if(age > 10 && age < 20) {
+//     console.log("Your age lies between 10 and 20");
+// }
+// else {
+//     console.log("Your age is not between 10 and 20");
+// }
+
+// **********************************************************************************
+// QUESTION 2 SOLUTION 
+
+let switch_age = prompt("What is your age?");
+
+switch(switch_age) {
+    case '12':
+        console.log("Your age is 12");
+        break;
+    case '13':
+        console.log("Your age is 13");
+        break;
+    case 14:
+        console.log("Your age is 14");
+        break;
+    case '15':
+        console.log("Your age is 15");
+        break;
+    case '16':
+        console.log("Your age is 16");
+        break;
+    default:
+        console.log("Your age is not special");
 }
