ec8b1423113a5802cb3b97670e641f0e099f5a82
Nikhil Narendra Chavan <chavannikhil802@gmail.com>
2023-09-30 14:07:51 +0530

    Updating the JS code for for-in loop

--- V9/09-for_loops.js
+++ V9/09-for_loops.js
@@ -1,10 +1,24 @@
-// PROGRAM TO ADD N NATURAL NUMBERS
+// PROGRAM TO ADD N NATURAL NUMBERS - FOR LOOP
 
-let sum = 0;
-let n = prompt("Enter the value of n");
-n = Number.parseInt(n);
+// let sum = 0;
+// let n = prompt("Enter the value of n");
+// n = Number.parseInt(n);
 
-for(let i=1; i<=n; i++) {
-    sum = sum + i;
+// for(let i=1; i<=n; i++) {
+//     sum = sum + i;
+// }
+// console.log("Sum of first "+n+" natural numbers is "+sum);
+// ------------------------------------------------------------------------
+// FOR IN LOOP
+
+let obj = {
+    harry: 90,
+    shubh: 45,
+    shivika: 80,
+    ritika: 57,
+    shiv: 99
 }
-console.log("Sum of first "+n+" natural numbers is "+sum);
+
+for(let a in obj) {
+    console.log("Marks of "+a+" are "+obj[a]);
+}
