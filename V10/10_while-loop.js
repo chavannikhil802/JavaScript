8e9f602b9578cff005905301146f00424a1da477
Nikhil Narendra Chavan <chavannikhil802@gmail.com>
2023-09-30 15:24:21 +0530

    Adding do-while loop code in JS file

--- V10/10_while-loop.js
+++ V10/10_while-loop.js
@@ -1,5 +1,4 @@
 // WHILE LOOP
-
 let n = prompt("Enter the value for n");
 n = Number.parseInt(n);
 
@@ -8,9 +7,12 @@ while(i<n) {
     console.log(i);
     i++;
 }
-
 // END OF WHILE LOOP
 
 // DO-WHILE LOOP
-
+do {
+    console.log("Do-While Loop - "+i);
+    i++;
+}
+while(i<n);
 // END OF DO-WHILE LOOP
