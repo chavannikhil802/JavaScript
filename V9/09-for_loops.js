46c6d965e7b4a7bcb4c36fe4ff0109026dd4ed6d
Nikhil Narendra Chavan <chavannikhil802@gmail.com>
2023-09-30 12:54:29 +0530

    Updating the JS code to add the sum of n natural numbers

--- V9/09-for_loops.js
+++ V9/09-for_loops.js
@@ -1,3 +1,10 @@
-for(let i=0; i<50; i++) {
-    console.log(i);
+// PROGRAM TO ADD N NATURAL NUMBERS
+
+let sum = 0;
+let n = prompt("Enter the value of n");
+n = Number.parseInt(n);
+
+for(let i=1; i<=n; i++) {
+    sum = sum + i;
 }
+console.log("Sum of first "+n+" natural numbers is "+sum);
