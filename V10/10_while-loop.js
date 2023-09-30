c5576914aa9755b556cbb0124ecdb8279bb22ab3
Nikhil Narendra Chavan <chavannikhil802@gmail.com>
2023-09-30 15:20:48 +0530

    Adding do-while loop info in txt file

---
+++ V10/10_while-loop.js
@@ -0,0 +1,16 @@
+// WHILE LOOP
+
+let n = prompt("Enter the value for n");
+n = Number.parseInt(n);
+
+let i=0;
+while(i<n) {
+    console.log(i);
+    i++;
+}
+
+// END OF WHILE LOOP
+
+// DO-WHILE LOOP
+
+// END OF DO-WHILE LOOP
