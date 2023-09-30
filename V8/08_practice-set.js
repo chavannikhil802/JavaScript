2e79d6d76f5b6e6fc8dc83fbb0a04c028c42bea9
Nikhil Narendra Chavan <chavannikhil802@gmail.com>
2023-09-30 12:00:07 +0530

    Adding solution to question 1 in JS file

---
+++ V8/08_practice-set.js
@@ -0,0 +1,9 @@
+// QUESTION 1 SOLUTION 
+
+let age = prompt("What is your age?");
+if(age > 10 && age < 20) {
+    console.log("Your age lies between 10 and 20");
+}
+else {
+    console.log("Your age is not between 10 and 20");
+}
