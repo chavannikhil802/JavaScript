43bd5d7e6fb63eed0187659d56a6fa8c03c03a34
Nikhil Narendra Chavan <chavannikhil802@gmail.com>
2023-09-30 12:12:07 +0530

    Adding solution to question 3 in JS file

--- V8/08_practice-set.js
+++ V8/08_practice-set.js
@@ -10,26 +10,40 @@
 // }
 
 // **********************************************************************************
+
 // QUESTION 2 SOLUTION 
 
-let switch_age = prompt("What is your age?");
+// let switch_age = prompt("What is your age?");
 
-switch(switch_age) {
-    case '12':
-        console.log("Your age is 12");
-        break;
-    case '13':
-        console.log("Your age is 13");
-        break;
-    case 14:
-        console.log("Your age is 14");
-        break;
-    case '15':
-        console.log("Your age is 15");
-        break;
-    case '16':
-        console.log("Your age is 16");
-        break;
-    default:
-        console.log("Your age is not special");
+// switch(switch_age) {
+//     case '12':
+//         console.log("Your age is 12");
+//         break;
+//     case '13':
+//         console.log("Your age is 13");
+//         break;
+//     case 14:
+//         console.log("Your age is 14");
+//         break;
+//     case '15':
+//         console.log("Your age is 15");
+//         break;
+//     case '16':
+//         console.log("Your age is 16");
+//         break;
+//     default:
+//         console.log("Your age is not special");
+// }
+
+// **********************************************************************************
+
+// QUESTION 3
+let num = prompt("Please enter a number");
+num = Number.parseInt(num);
+
+if(num%2 == 0 && num%3 == 0) {
+    console.log(num+" is divisible by 2 and 3");
+}
+else {
+    console.log(num+" is not divisible by 2 and 3");
 }
