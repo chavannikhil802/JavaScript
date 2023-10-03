82da4f62035a823d94f1d3cec16bf66ecdd69b40
Nikhil Narendra Chavan <chavannikhil802@gmail.com>
2023-10-03 12:39:04 +0530

    Adding solution to question 2

--- V12/12-practice_set.js
+++ V12/12-practice_set.js
@@ -5,8 +5,13 @@ let marks = {
     monika: 5
 }
 
-
 // QUESTION 1 SOLUTION
 for(let i = 0; i < Object.keys(marks).length; i++) {
     console.log("The marks of "+Object.keys(marks)[i]+" are "+marks[Object.keys(marks)[i]])
+}
+
+// QUESTION 2 SOLUTION
+for(let key in marks) {
+    // console.log("The marks of "+Object.keys(marks)[i]+" are "+marks[Object.keys(marks)[i]])
+    console.log("The marks of "+key+" are "+marks[key])
 }
