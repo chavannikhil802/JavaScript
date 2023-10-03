015330875dbd998fbd2af1c7c6b1f1e3f5091f92
Nikhil Narendra Chavan <chavannikhil802@gmail.com>
2023-10-03 11:59:08 +0530

    Creating a function in JS file

---
+++ V11/11_function.js
@@ -0,0 +1,11 @@
+let a = 1;
+let b = 2;
+let c = 3;
+
+function onePlusAvg(x, y) {
+    return 1 + (x + y)/2;
+}
+
+console.log("one plus Average of a and b is ", onePlusAvg(a, b));
+console.log("one plus Average of a and b is ", onePlusAvg(b, c));
+console.log("one plus Average of a and b is ", onePlusAvg(a, c));
